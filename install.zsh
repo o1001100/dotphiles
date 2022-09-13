fc4ea2c229cb9d02e36c78953cddc9759ac1ca60
L Tscharke <32327925+winkwonkbitch@users.noreply.github.com>
2022-09-13 13:20:02 +1000

    quick fixes to apt handling and package naming

--- install.zsh
+++ install.zsh
@@ -12,13 +12,34 @@ function testing_crap () {
 # checking user
 if [[ $(whoami) = 'root' ]]; then print "Don't run as root!" && exit 1; else; fi
 
+# checking distro
+if [[ (-f /etc/os-release) ]]
+then
+  . /etc/os-release
+  local distro=$ID
+  if [[ ($distro = 'debian' || 'arch') ]]
+  then
+    print "\nRunning on $NAME \n"
+  else
+    print 'Unknow/unsupported distro, exiting'
+    exit 1
+  fi
+else
+  print 'Unknown/unsupported distro, exiting'
+  exit 1
+fi
+
 # setting variables
 local missa=('')
+local missc=('')
 local missp=('')
 local rsh=(false)
 local dots=$(pwd)
+local cargo='cargo install'
+if [[ ($distro = debian) ]]; then local pkgman='sudo apt-get install -y'; elif [[ ($distro = arch) ]]; then local pkgman='paru -S --noconfirm --needed'; fi
 
 function install_paru () {
+  if [[ ($distro != arch) ]]; then return; fi
   print "Paru doesn't appear to be installed, would you like me to install it for you? (Y/n)"
   read -sq place
   if [[ ($place = 'y') ]]
@@ -27,6 +48,23 @@ function install_paru () {
     sudo pacman -S --needed --noconfirm base-devel git
     git clone https://aur.archlinux.org/paru.git /tmp/paru
     ( cd paru && makepkg -si )
+    print '\nFinished, continuing installer\n'
+  else
+    print 'Okay buddy'
+    exit 0
+  fi
+}
+
+function install_pacstall () {
+  if [[ ($distro != debian) ]]; then return; fi
+  print "Pacstall doesn't appear to be installed, would you like me to install it for you? (Y/n)"
+  read -sq place
+  if [[ ($place = 'y') ]]
+  then
+    print 'Okay, installing Pacstall'
+    sudo apt-get install -y curl wget
+    sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+    print '\nFinished, continuing installer\n'
   else
     print 'Okay buddy'
     exit 0
@@ -34,38 +72,34 @@ function install_paru () {
 }
 
 function install_omz () {
-  if $([ ! -d "$HOME/.oh-my-zsh" ])
+  if $([ -d "$HOME/.oh-my-zsh" ]); then return; fi
+  print "Oh My Zsh doesn't appear to be installed, would you like me to install it for you? (Y/n)"
+  read -sq place
+  if [[ ($place = 'y') ]]
   then
-    print "Oh My Zsh doesn't appear to be installed, would you like me to install it for you? (Y/n)"
-    read -sq place
-    if [[ ($place = 'y') ]]
-    then
-      print 'Okay, installing Oh My Zsh'
-      sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-      print '\nFinished! You now need to log out of your current shell session and log back in before you can run this script again'
-      exit 0
-    else
-      print 'Okay buddy'
-      exit 0
-    fi
+    print 'Okay, installing Oh My Zsh'
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    print '\nFinished! You now need to log out of your current shell session and log back in before you can run this script again'
+    exit 0
+  else
+    print 'Okay buddy'
+    exit 0
   fi
 }
 
 function install_p10k () {
-  if $([ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ])
+  if $([ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]); then return; fi
+  print "Powerlevel10k doesn't appear to be installed, would you like me to install it for you? (Y/n)"
+  read -sq place
+  if [[ ($place = 'y') ]]
   then
-    print "Powerlevel10k doesn't appear to be installed, would you like me to install it for you? (Y/n)"
-    read -sq place
-    if [[ ($place = 'y') ]]
-    then
-      print 'Okay, installing Powerlevel10k'
-      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-      print '\nFinished, continuing installer\n'
-      rsh=(true)
-    else
-      print 'Okay buddy'
-      exit 0
-    fi
+    print 'Okay, installing Powerlevel10k'
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    print '\nFinished, continuing installer\n'
+    rsh=(true)
+  else
+    print 'Okay buddy'
+    exit 0
   fi
 }
 
@@ -84,35 +118,41 @@ function install_cargo () {
   fi
 }
 
-# checking for packages
-#if [[ $(command -v paru) = "" ]]; then install_paru; fi
-#if [[ $(command -v omz) = "" ]]; then install_omzsh; fi
-#if $([ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]); then install_p10k; fi
-#if [[ $(command -v cargo) = "" ]]; then install_rust; fi
-#if [[ $(command -v npm) = "" ]]; then install_npm; fi
-#if [[ ($rsh = true) ]]; then print 'You now need to log out of your current shell session and log back in before you can run this script again'; exit 0; fi
-#if $([ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]); then missp=(zsh-autosuggestions $missp); fi
-#if $([ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]); then missp=(zsh-syntax-highlighting $missp); fi
+# check for installer dependencies
 
-# check for installed prerequisites
-
-for f in $(<prq.pkgs)
+for f in $(<dep.pkgs)
 do
   if [[ $(command -v "$f") = "" ]]; then install_$f; fi
 done
 
-# check for installed plugins
+# check for plugins
 
 for f in $(<plg.pkgs)
 do
   if $([ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$f" ]); then missp=($f $missp); fi
 done
 
-# check for installed packages
+# check for packages in distro package manager
 
-for f in $(<aur.pkgs)
+if [[ ($distro = *'arch'*) ]]
+then
+  for f in $(<aur.pkgs)
+  do
+    if [[ $(paru -Q) != *"$f"* ]]; then missa=($f $missa); fi
+  done
+elif [[ ($distro = *'debian'*) ]]
+then
+  for f in $(<apt.pkgs)
+  do
+    if [[ $(apt list -i) != *"$f"* ]]; then missa=($f $missa); fi
+  done
+fi
+
+# check for cargo packages
+
+for f in $(<cargo.pkgs)
 do
-  if [[ $(paru -Q) != *"$f"* ]]; then missa=($f $missa); fi
+  if [[ $(<aur.pkgs) != *"$f"* && $(eval $cargo --list) != *"$f"* ]]; then missc=($f $missc); fi
 done
 
 # promt user to restart shell session if needed
@@ -134,7 +174,7 @@ function place_dots () {
   rsync -crv $dots/themes/. $HOME/.themes
   rsync -crv $dots/bin/. /usr/local/bin
   print '\nSymlinking ZSH config'
-  if $([ ! -d '~/.zshrc' ]); then ln -s '~/.config/zsh/zshrc' '~/.zshrc'; fi
+  if $([ ! -d '~/.zshenv' ]); then ln -s '~/.config/zsh/.zshenv' '~/.zshenv'; fi
   print 'All done, quitting installer'
   exit 0
 }
@@ -144,17 +184,22 @@ function install_packages () {
   print '\nInstalling all required packages'
   if [[ ($missa != '') ]]
   then
-    paru -S $missa --noconfirm
+    eval $pkgman $missa
     if [[ ($missa = *"tailscale"*) ]]; then sudo systemctl enable --now tailscaled; fi
-  else; fi
+    if [[ ($missa = *"tealdeer"*) ]]; then cp completion/zsh_tealdeer /usr/share/zsh/site-functions/_tldr; fi
+  fi
+  if [[ ($missc != '') ]]
+  then
+    eval $cargo $missc
+  fi
   if [[ ($missp != '') ]]
   then
     for f in $missp
     do
       git clone https://github.com/zsh-users/$f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/$f
     done
-    print 'You will need to restart your current shell to make new plugins availiable'
-  else; fi
+    print '\nYou will need to restart your current shell to make new plugins availiable\n'
+  fi
   if [[ ($place = 'y') ]]
   then
     print 'All missing packages have been installed, continuing'
