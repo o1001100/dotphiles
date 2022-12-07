8a40d5c3dd3c0a178a3939664fa8b38ca36d6c63
01001100 <32327925+winkwonkbitch@users.noreply.github.com>
2022-12-07 14:45:14 +1100

    kinda works for now

--- install.zsh
+++ install.zsh
@@ -3,13 +3,13 @@
 function title () {
   export lcommit=`git log -1`
   printf '%.s─' $(seq 1 $(tput cols))
-  echo "\n     _       _         _     _ _           "
-  echo "    | |     | |       | |   (_) |          "
-  echo "  __| | ___ | |_ _ __ | |__  _| | ___  ___ "
-  echo " / _  |/ _ \| __| '_ \| '_ \| | |/ _ \/ __|"
-  echo "| (_| | (_) | |_| |_) | | | | | |  __/\__ \ "
-  echo " \__,_|\___/ \__| .__/|_| |_|_|_|\___||___/"
-  echo "                 | |                        "
+  echo "\n     _       _         _     _ _                "
+  echo "    | |     | |       | |   (_) |                 "
+  echo "  __| | ___ | |_ _ __ | |__  _| | ___  ___        "
+  echo " / _  |/ _ \| __| '_ \| '_ \| | |/ _ \/ __|       "
+  echo "| (_| | (_) | |_| |_) | | | | | |  __/\__ \       "
+  echo " \__,_|\___/ \__| .__/|_| |_|_|_|\___||___/       "
+  echo "                 | |                              "
   echo "                 |_|      Running on: $PRETTY_NAME"
   echo "\n\nLatest Commit:"
   echo $lcommit"\n"
@@ -25,36 +25,17 @@ function dotphile_update () {
   else
     print 'Collecting updates\n'
   fi
+  #dots=$DOTS_LOCATION
   cd $dots && git pull --recurse-submodules
-  if [[ ($DOTS_TYPE = 'full') ]]
-  then
-    print "Found install type, updating your installation"
-    for f in $(<$dots/configs/full.dir)
-    do
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-      rsync -crvl $dots/dirs/.zpreztorc $HOME/.config/zsh/.zpreztorc
-    done
-  elif [[ ($DOTS_TYPE = 'lite') ]]
-  then
-    print "Found install type, updating your installation"
-    for f in $(<$dots/configs/lite.dir)
-    do
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-      rsync -crvl $dots/dirs/.zpreztorclite $HOME/.config/zsh/.zpreztorc
-    done
-  elif [[ ($DOTS_TYPE = 'termux') ]]
-  then
-    print "Found install type, updating your installation"
-    for f in $(<$dots/configs/tmx.dir)
-    do
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-      rsync -crvl $dots/dirs/.zpreztorclite $HOME/.config/zsh/.zpreztorc
-    done
-  fi
-  if [[ ($full = true) ]]; then type='full'; elif [[ ($distro = 'termux') ]]; then type='termux'; else type='lite'; fi
+  print "Found install type, updating your installation"
+  for f in $(<$dots/configs/serve.dir)
+  do
+    rsync -crvl $dots/dirs/$f/. $HOME/.$f
+  done
+  rsync -crvl $dots/dirs/.zpreztorc $HOME/.config/zsh/.zpreztorc
   envar="
 # setting variables for installer
-export DOTS_TYPE='$type'
+export DOTS_TYPE='serve'
 export DOTS_LOCATION='$dots'"
   echo $envar >> $HOME/.zshenv
   print '\nFinished with dots, checking for new/updated packages\n'
@@ -67,16 +48,12 @@ Install files and programs configured in the config directory.
 
 With no FLAG(s), will try to detect distro and to install if distro is supported.
 
-  -f, --flags <distro>    skips distro checks and installs assuming user provided distro
-  -a, --arch              attempts to install assuming distro is Arch Linux
-  -d, --debian            attempts to install assuming distro is Debian GNU/Linux
-  -t, --termux            attempts to install assuming distro is Android (through Termux)
+  -f, --force             skips checks and installs anyway
   -u, --update            updates installed submodules (also accessible from the 'dotphile-update' command
   -h, --help              shows this help
 
 Examples:
-  ./install.zsh -f debian
-  ./install.zsh --arch
+  ./install.zsh -f
   ./install.zsh --update
 
 Written and provided by 01001100: <https://github.com/winkwonkbitch>"
@@ -94,43 +71,17 @@ Try './install.zsh --help' for more information."
 
 ######################################################################
 
-function install_paru () {
-  if [[ ($distro != arch) ]]; then return; fi
-  print "Paru doesn't appear to be installed, would you like me to install it for you? (Y/n)"
-  read -sq place
-  if [[ ($place = 'y') ]]
-  then
-    print 'Okay, installing Paru'
-    sudo pacman -S --needed --noconfirm base-devel git
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    ( cd paru && makepkg -si )
-    print '\nFinished, continuing installer\n'
-  else
-    print 'Okay buddy'
-    exit 0
-  fi
-}
-
 # installing Rust and Cargo
 function install_cargo () {
   print "Rust doesn't appear to be installed, would you like me to install it for you? (Y/n)"
   read -sq place
-  if [[ ($place = 'y') && ($distro != 'termux')  ]]
+  if [[ ($place = 'y') ]]
   then
     print 'Okay, installing Rust'
-    if [[ ($distro = 'arch'*) ]]
-    then
-      eval $pkgman base-devel
-    elif [[ ($distro = 'debian') ]]
-    then
-      eval $pkgman build-essential
-    fi
+    eval $pkgman build-essential
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     print '\nFinished, continuing installer\n'
     rsh=true
-  elif [[ ($place = 'y') && ($distro = 'termux') ]]
-  then
-    eval $pkgman build-essential rust
   else
     print 'Okay buddy'
     exit 0
@@ -140,39 +91,21 @@ function install_cargo () {
 # placing dotfiles
 function place_dots () {
   print '\n Setting up directories and placing files'
-  if [[ ($full = true) ]]
-  then
-    for f in $(<$dots/configs/full.dir)
-    do
-      if $([ ! -d $HOME/.$f ]); then mkdir $HOME/.$f; fi
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-    done
-  elif [[ ($full = false) && ($distro = 'termux') ]]
-  then
-    if $([ ! -d $HOME/.config ]); then mkdir $HOME/.config; fi
-    for f in $(<$dots/configs/tmx.dir)
-    do
-      if $([ ! -d $HOME/.$f ]); then mkdir $HOME/.$f; fi
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-    done
-  else
-    if $([ ! -d $HOME/.config ]); then mkdir $HOME/.config; fi
-    for f in $(<$dots/configs/lite.dir)
-    do
-      if $([ ! -d $HOME/.$f ]); then mkdir $HOME/.$f; fi
-      rsync -crvl $dots/dirs/$f/. $HOME/.$f
-    done
-  fi
-
+  for f in $(<$dots/configs/serve.dir)
+  do
+    if $([ ! -d $HOME/.$f ]); then mkdir -p $HOME/.$f; fi
+    rsync -crvl $dots/dirs/$f/. $HOME/.$f
+  done
+  rsync -crvl $dots/dirs/.zpreztorc $HOME/.config/zsh/.zpreztorc
   print '\nSymlinking ZSH config'
   if $([ -f $HOME/.zshenv ]); then rm -v $HOME/.zshenv; fi
   if $([ ! -f $HOME/.zshenv ]); then ln -sv $HOME/.config/zsh/.zshenv $HOME/.zshenv; fi
 
   print '\nSetting environmental variables for future installs'
-  if [[ ($full = true) ]]; then type='full'; elif [[ ($distro = 'termux') ]]; then type='termux'; else type='lite'; fi
+  type='serve'
   envar="
 # setting variables for installer
-export DOTS_TYPE='$type'
+export DOTS_TYPE='serve'
 export DOTS_LOCATION='$dots'"
   echo $envar >> $HOME/.zshenv
 
@@ -185,9 +118,17 @@ function install_packages () {
   print '\nInstalling all required packages'
   if [[ ($missa != '') ]]
   then
-    if [[ ($missa = *"tailscale"*) && ($distro = *"debian"*) ]]; then curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null && curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list && sudo apt-get update; fi
+    if [[ ($missa = *"tailscale"*) ]]; then curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null && curl -fsSL https://pkgs.tailscale.com/stable/debian/sid.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list && sudo apt-get update; fi
+    if [[ ($missa = *"docker"*) ]]; then curl -sSL https://get.docker.com/ | sudo sh; fi
     eval $pkgman $missa
-    if [[ ($missa = *"tailscale"*) ]]; then sudo systemctl enable --now tailscaled; fi
+    if [[ ($missa = *"tailscale"*) ]]; then 
+      echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+      echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+      sudo sysctl -p /etc/sysctl.conf
+      sudo tailscale up --advertise-exit-node --ssh
+      sudo systemctl enable --now tailscaled
+    fi
+    if [[ ($missa = *"docker"* ) ]]; then sudo usermod -aG docker $(whoami); fi
     if [[ ($missa = *"tealdeer"*) ]]; then cp completion/zsh_tealdeer /usr/share/zsh/site-functions/_tldr; fi
   fi
   if [[ ($missc != '') ]]
@@ -204,19 +145,9 @@ function install_packages () {
 # updating packages
 function pkg_update () {
   print '\nUpdating all installed packages'
-  if [[ ($distro = 'arch'*) ]]
-  then
-    print '\nUpdating all packages installed with Paru'
-    paru
-  elif [[ ($distro = 'debian') ]]
-  then
-    print '\nUpdating all packages installed with APT'
-    sudo apt-get update && sudo apt-get full-upgrade
-  elif [[ ($distro = 'termux') ]]
-  then
-    print '\nUpdating all packages installed with PKG'
-    pkg update
-  fi
+  print '\nUpdating all packages installed with APT'
+  sudo apt-get update && sudo apt-get full-upgrade
+  print '\nUpdating all packages installed with Cargo'
   cargo install-update -a
   print 'Everything should now be up to date, exiting'
   exit 0
@@ -237,24 +168,19 @@ function initial_setup () {
   then
     . /etc/os-release
     if [[ ($force != true) ]]; then distro=$ID; fi
-    if [[ ($distro != 'arch'*) && ($distro != 'debian') ]]
+    if [[ ($distro != 'debian') ]]
     then
-      print 'This installer currently only supports Debian Linux, Arch Linux, and Termux'
+      print 'This installer is intended for use on a server and currently only supports Debian Linux'
       print "Distro detected: $PRETTY_NAME"
       print 'The script will now exit, please try again on a supported distro or install manually'
       exit 1
-    elif [[ ($distro = *'debian'*) && ($PRETTY_NAME != *'sid'*) ]]
+    elif [[ ($distro = *'debian'*) && ($PRETTY_NAME != *'bullseye'*) ]]
     then
-      print 'This script currently only supports Debian Sid (unstable)'
+      print 'This script is intended for use on a server and currently only supports Debian Bullseye (stable)'
       print "Version detected: $PRETTY_NAME"
       print 'The script will now exit, please try again on a supported version or install manually'
       exit 1
     fi
-  elif [[ $(command -v termux-reload-settings) ]]
-    then
-    distro='termux'
-    PRETTY_NAME='Android x64 (through Termux)'
-    full=false
   else
     print 'Unknown/unsupported distro, exiting'
     exit 1
@@ -266,8 +192,7 @@ function initial_setup () {
   rsh=(false)
   dots=$(pwd)
   cargo=('cargo install')
-  full=(false)
-  if [[ ($distro = 'debian') ]]; then pkgman='sudo apt-get install -y'; elif [[ ($distro = 'arch'*) ]]; then pkgman='paru -S --noconfirm --needed'; elif [[ ($distro = 'termux') ]]; then pkgman='pkg install -y'; fi
+  pkgman='sudo apt-get install -y'
 }
 
 function package_setup () {
@@ -278,67 +203,19 @@ function package_setup () {
   done
 
   # check for packages in distro package manager
-  if [[ ($distro = *'arch'*) && ($full = true) ]]
-  then
-    for f in $(<$dots/configs/aur.pkgs)
-    do
-      if [[ $(paru -Q) != *"$f"* ]]; then missa=($f $missa); fi
-    done
-  elif [[ ($distro = *'arch'*) && ($full = false) ]]
-  then
-    for f in $(<$dots/configs/aur-lite.pkgs)
-    do
-      if [[ $(paru -Q) != *"$f"* ]]; then missa=($f $missa); fi
-    done
-  elif [[ ($distro = *'debian'*) && ($full = true) ]]
-  then
-    for f in $(<$dots/configs/apt.pkgs)
-    do
-      if [[ $(dpkg --get-selections | grep -v deinstall) != *"$f"* ]]; then missa=($f $missa); fi
-    done
-  elif [[ ($distro = *'debian'*) && ($full = false) ]]
-  then
-    for f in $(<$dots/configs/apt-lite.pkgs)
-    do
-      if [[ $(dpkg --get-selections | grep -v deinstall) != *"$f"* ]]; then missa=($f $missa); fi
-    done
-  elif [[ ($distro = *'termux'*) ]]
-  then
-    for f in $(<$dots/configs/pkg.pkgs)
-    do
-      if [[ $(dpkg --get-selections | grep -v deinstall) != *"$f"* ]]; then missa=($f $missa); fi
-    done
-  fi
+  for f in $(<$dots/configs/apt.pkgs)
+  do
+    if [[ $(dpkg --get-selections | grep -v deinstall) != *"$f"* ]]; then missa=($f $missa); fi
+  done
 
   # check for cargo packages
-  if [[ ($full = true) ]]
-  then
-    for f in $(<$dots/configs/cargo.pkgs)
-    do
-      if [[ ($(<$dots/configs/aur.pkgs) != *"$f"*) && ($(eval $cargo --list) != *"$f"*) && ($distro = *'arch'*) ]]
-      then
-        missc=($f $missc)
-      elif [[ ($(eval $cargo --list) != *"$f"*) && ($distro != *'arch'*) ]]
-      then
-        missc=($f $missc)
-      fi
-    done
-  elif [[ ($full = false) ]]
-  then
-    for f in $(<$dots/configs/cargo-lite.pkgs)
-    do
-      if [[ ($(<$dots/configs/aur-lite.pkgs) != *"$f"*) && ($(eval $cargo --list) != *"$f"*) && ($distro = *'arch'*) ]]
-      then
-        missc=($f $missc)
-      elif [[ ($(<$dots/configs/pkg.pkgs) != *"$f"*) && ($(eval $cargo --list) != *"$f"*) && ($distro = *'termux'*) ]]
-      then
-        missc=($f $missc)
-      elif [[ ($(eval $cargo --list) != *"$f"*) && ($distro != *'arch'*) && ($distro != *'termux'*) ]]
-      then
-        missc=($f $missc)
-      fi
-    done
-  fi
+  for f in $(<$dots/configs/cargo.pkgs)
+  do
+    if [[ ($(eval $cargo --list) != *"$f"*) ]]
+    then
+      missc=($f $missc)
+    fi
+  done
 
   # promt user to restart shell session if needed
   if [[ ($rsh = true) ]]; then print 'You now need to log out of your current shell session and log back in before you can run this script again'; exit 0; fi
@@ -349,13 +226,6 @@ function package_setup () {
 ###                        User preferences                        ###
 
 ######################################################################
-
-# choose between full and lite installation
-function lite () {
-  print "Install extra (gui) components? (Y/n)"
-  read -sq place
-  if [[ ($place = 'y') ]]; then full=true; else full=false; fi
-}
 
 # promt to place dotfiles
 function dots () {
@@ -402,7 +272,6 @@ function packages () {
 function dewwit () {
   initial_setup
   title
-  if [[ ($distro != 'termux') ]]; then lite; fi
   package_setup
   if [[ ($missa = '' && $missc = '') ]]
   then
